53f1b80dc6dffe3bb78b704a735754363d5e29b1
klfz <1563429060@qq.com>
2019-11-06 16:10:25 +0800

    基础搭建

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav-bar">
+     <div class="left"> <slot name="left"></slot> </div>
+     <div class="centen">  <slot name="centen"></slot>  </div>
+     <div class="right"> <slot name="right"></slot> </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'NavBar'
+}
+</script>
+
+<style>
+.nav-bar
+{
+    display: flex;
+    text-align: center;
+    line-height: 44px;
+    color: white;
+    background-color: pink;
+    box-shadow: 0px 1px 1px rgba(100,100,100, .1)
+}
+.left{
+    width: 45px;
+}
+.centen{
+    flex: 1;
+}
+.right{
+    width: 45px;
+    background-color:pink; 
+}
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="home">
+    <NavBar>
+      <template v-slot:centen>
+        <div>购物街</div>
+      </template>
+    </NavBar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+
+
+export default {
+  name:'home',
+  components:{
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
