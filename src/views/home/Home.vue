9b2077064f02250e33fcf914b607add705775c0f
klfz <1563429060@qq.com>
2019-11-06 17:06:51 +0800

    轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,17 +5,34 @@
         <div>购物街</div>
       </template>
     </NavBar>
+    <HomeSwiper :cbanners='banners'></HomeSwiper>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
-
+import HomeSwiper from "./childComps/HomeSwiper"
+import { getHomeMultidata } from "network/home"
 
 export default {
-  name:'home',
+   name:'home',
+  data(){
+    return{
+      banners:[],
+      recommends:[]
+    }
+  }, 
   components:{
-    NavBar
+    NavBar,
+    HomeSwiper
+  },
+  created(){
+    getHomeMultidata().then(res=>{
+     this.banners=res.data.data.banner.list;
+     console.log(this.banners);
+     this.recommends=res.data.data.recommend.list;
+
+    })
   }
 }
 </script>
